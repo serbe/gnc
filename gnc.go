4894589cb73cb9daccea12392506008da2b0fd2c
serbe <mkoctuk@gmail.com>
2016-03-28 10:34:37 +0300

    add file operations

--- gnc.go
+++ gnc.go
@@ -7,6 +7,8 @@ import (
     "bytes"
     "strconv"
     "io/ioutil"
+    "bufio"
+    "os"
 )
 
 type postData struct {
@@ -31,35 +33,83 @@ type postResult struct {
    	Locale  string `json:"Locale"`
 }
 
-func main() {
-    var client http.Client
-    var data postData
+func readLines(path string) ([]string, error) {
+  file, err := os.Open(path)
+  if err != nil {
+    return nil, err
+  }
+  defer file.Close()
+
+  var lines []string
+  scanner := bufio.NewScanner(file)
+  for scanner.Scan() {
+    lines = append(lines, scanner.Text())
+  }
+  return lines, scanner.Err()
+}
+
+func writeLine(line string, filename string) error {
+  file, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY, 0600)
+  if err != nil {
+    return err
+  }
+  defer file.Close()
+
+  w := bufio.NewWriter(file)
+  fmt.Fprintln(w, line)
+  return w.Flush()
+}
+
+func getResponce(name string) (postResult, error) {
+    var ( 
+    client http.Client
+    data postData
+    result postResult
+    )
     postURL := "https://accounts.google.com/InputValidator?resource=SignUp"
-    data.Input01.GmailAddress = "oldhurd"
+    data.Input01.GmailAddress = name
     data.Input01.Input = "GmailAddress"
     data.Locale = "ru"
-    postJson, err := json.Marshal(data)
+    postJ, err := json.Marshal(data)
     if err != nil {
-        panic(err)
+        return result, err
     }
-    req, _ := http.NewRequest("POST", postURL, bytes.NewBufferString(string(postJson)))
+    req, _ := http.NewRequest("POST", postURL, bytes.NewBufferString(string(postJ)))
 	req.Header.Add("Content-Type", "application/json")
-    req.Header.Add("Content-Length", strconv.Itoa(len(string(postJson))))
+    req.Header.Add("Content-Length", strconv.Itoa(len(string(postJ))))
     resp, err := client.Do(req)
     if err != nil {
-		panic(err)
+		return result, err
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
     jsonResult, err := json.Marshal(string(body)) 
     if err != nil {
-		panic(err)
+		return result, err
 	}
     jsonResultString, err := strconv.Unquote(string(jsonResult)) 
     if err != nil {
-		panic(err)
+		return result, err
 	}
-    var result postResult
+    
     err = json.Unmarshal([]byte(jsonResultString), &result)
-    fmt.Println(result.Input01.ErrorData, result.Input01.Valid)
+    return result, err
+}
+
+func main() {
+    lines, err := readLines("words.txt")
+    if err != nil {
+        panic(err)
+    }
+    for _, word := range lines {
+        if len(word) == 6 {
+            responce, err := getResponce(word)
+            if err != nil {
+                writeLine(word + ", ", "err.txt")
+            }
+            if responce.Input01.Valid == "true" {
+                writeLine(word + ", ", "valid.txt")
+            }
+        }
+    }
 }
